2f218732fbd25bc31d03319cd9dd402382eebe7b
deG3nt3lm4n <th3g3nt3lm4n@gmail.com>
2021-04-13 16:43:31 -0400

    database + model working

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,21 @@
 // DO YOUR MAGIC
+exports.up = function(knex) {
+  return knex.schema.createTable("cars", tbl => {
+    tbl.increments(); //primary key
+    tbl.string('vin')
+      .notNullable()
+      .unique();
+    tbl.string('make')
+      .notNullable();
+    tbl.string('model')
+      .notNullable();
+    tbl.integer('mileage')
+      .notNullable();
+    tbl.string('title');
+    tbl.string('transmission');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
+};
